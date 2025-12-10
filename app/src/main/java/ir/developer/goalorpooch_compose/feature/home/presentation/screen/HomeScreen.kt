60a871bd4ec5885a357a8479a9ea489f3d4dc93c
SaraSaber <saraasaber77@gmail.com>
2025-12-10 14:23:54 +0330

    feat(screen home)

---
+++ app/src/main/java/ir/developer/goalorpooch_compose/feature/home/presentation/screen/HomeScreen.kt
@@ -0,0 +1,21 @@
+package ir.developer.goalorpooch_compose.feature.home.presentation.screen
+
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+
+    Scaffold() {
+
+    }
+
+}
+
+@Preview
+@Composable
+private fun HomeScreenPreview() {
+    HomeScreen()
+}
