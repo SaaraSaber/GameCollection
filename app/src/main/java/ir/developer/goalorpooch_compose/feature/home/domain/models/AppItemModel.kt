5098ca96f8d64547e63c5f73657d6ef6cf42c2f0
SaraSaber <saraasaber77@gmail.com>
2025-12-10 14:22:03 +0330

    feat(data models) -add dataModel

---
+++ app/src/main/java/ir/developer/goalorpooch_compose/feature/home/domain/models/AppItemModel.kt
@@ -0,0 +1,8 @@
+package ir.developer.goalorpooch_compose.feature.home.domain.models
+
+data class AppItemModel(
+    val id: Int,
+    val name: String,
+    val description: String,
+    val iconRes: Int
+)
---
+++ app/src/main/java/ir/developer/goalorpooch_compose/feature/home/domain/models/ShopItemModel.kt
@@ -0,0 +1,9 @@
+package ir.developer.goalorpooch_compose.feature.home.domain.models
+
+data class ShopItemModel(
+    val id: Int,
+    val coinAmount: Int,
+    val price: Int,
+    val discount: Int,
+    val iconRes: Int
+)
